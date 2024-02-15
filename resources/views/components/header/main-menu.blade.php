a47be1e8a6714d176d730f0c00393fa5adf95454
asmi046 <asmi046@gmail.com>
2024-02-15 16:23:32 +0300

    +1

--- resources/views/components/header/main-menu.blade.php
+++ resources/views/components/header/main-menu.blade.php
@@ -1,13 +1,11 @@
-<section class="menu_section">
-    <div class="header-menu">
-      <div class="container">
+<section class="header-menu">
+        <div class="container">
             <ul>
                 <li><a href="{{route('catalog')}}">Каталог</a></li>
-                <li><a href="#">Обувь, берцы</a></li>
-                <li><a href="#">Головные уборы</a></li>
-                <li><a href="#">Снаряжение</a></li>
-                <li><a href="#">Одежда</a></li>
             </ul>
-      </div>
-    </div>
+
+            <a href="#">Избранное (<favorites-counter></favorites-counter>)</a>
+        </div>
+
+
 </section>
