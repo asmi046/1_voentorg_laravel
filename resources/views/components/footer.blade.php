a82bda0a51ea29ae20e31ec23bb48c2c28b63bd1
asmi046 <asmi046@gmail.com>
2024-02-16 11:33:00 +0300

    Корректировка подвала

--- resources/views/components/footer.blade.php
+++ resources/views/components/footer.blade.php
@@ -1,50 +1,42 @@
 <footer class="footer">
     <div class="container">
-      <div class="footer-block">
+        <div class="footer-block">
+
+            <div class="footer-title"><h2>Контакты</h2></div>
+
+            @foreach ($shops as $item)
+                <ul>
+                    <li>
+                        <strong>
+                            {{ $item->name }}
+                        </strong>
+                    </li>
+                    <li>
+                        <span class="footer-address adr_b">
+                            {{ $item->adress }}
+                            @if (!empty($item->orientir))
+                                ({{ $item->orientir }})
+                            @endif
+
+                        </span>
+                    </li>
+                    <li><a class="callback__phone" href="tel:+7{{phone_format($item->phone)}}">{{ $item->phone }}</a></li>
+                </ul>
+            @endforeach
+
+        </div>
+
+        <div class="footer-block">
         <div class="footer-title"><h2>Информация</h2></div>
         <ul>
             <x-smile-menu-puncts></x-smile-menu-puncts>
         </ul>
       </div>
-      <div class="footer-block">
-        <div class="footer-title"><h2>Каталог товаров</h2></div>
 
-         <ul>
-            <li><a href="{{route('catalog')}}">Каталог</a></li>
-            <li><a href="#">Обувь, берцы</a></li>
-            <li><a href="#">Головные уборы</a></li>
-            <li><a href="#">Снаряжение</a></li>
-         </ul>
-
-      </div>
       <div class="footer-block">
           <div class="footer-title"><h2>Мы в соцсетях</h2></div>
           <x-social-icon></x-social-icon>
       </div>
-      <div class="footer-block">
 
-        <div class="footer-title"><h2>Контакты</h2></div>
-
-        @foreach ($shops as $item)
-            <ul>
-                <li>
-                    <strong>
-                        {{ $item->name }}
-                    </strong>
-                </li>
-                <li>
-                    <span class="footer-address adr_b">
-                        {{ $item->adress }}
-                        @if (!empty($item->orientir))
-                            ({{ $item->orientir }})
-                        @endif
-
-                    </span>
-                </li>
-                <li><a class="callback__phone" href="tel:+7{{phone_format($item->phone)}}">{{ $item->phone }}</a></li>
-            </ul>
-        @endforeach
-
-      </div>
     </div>
   </footer>
