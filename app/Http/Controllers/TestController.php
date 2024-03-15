01d0cc304a2d8e508e4337867e6f4c62384bb3e7
asmi046 <asmi046@gmail.com>
2024-03-15 11:30:57 +0300

    +new wm

--- app/Http/Controllers/TestController.php
+++ app/Http/Controllers/TestController.php
@@ -14,7 +14,7 @@ class TestController extends Controller
     public function index() {
         $manager = new ImageManager(new Driver());
         $img = $manager->read(public_path('watermark/t3.jpg'));
-        $img_wm = $manager->read(public_path('watermark/wm_1000.png'));
+        $img_wm = $manager->read(public_path('watermark/wm_1000_1.png'));
 
         $wm_pos_right = ($img->width() - $img_wm->width())/2;
         $wm_pos_bottom = ($img->height() - $img_wm->height())/2;
