5d38caa9dad8bc89822539415b53b8ba5d439b66
asmi046 <asmi046@gmail.com>
2024-05-03 10:02:59 +0300

    телефон менеджерв

--- resources/views/components/header/controll.blade.php
+++ resources/views/components/header/controll.blade.php
@@ -14,7 +14,11 @@
         </a>
 
         <div class="header-contacts">
-            <a href="tel:+74712730499">  <span class=red> 7 (4712) 730-449 </span> </a>	<div class="address">г. Курск, ул. Верхняя Луговая, 6 </div>
+            <a href="tel:+74712730499">  <span class=red> +7 (4712) 730-449 </span> </a>	<div class="address">г. Курск, ул. Верхняя Луговая, 6 </div>
+        </div>
+
+        <div class="header-contacts">
+            <a href="tel:+79510849233">  <span class=red> +7 (951) 084-92-33 </span> </a>	<div class="address">Отдел продаж</div>
         </div>
 
 
