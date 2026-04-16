0d91f378e7e113ad5362b8c9fba5c61987ada339
asmi046 <asmi046@gmail.com>
2026-04-16 17:15:58 +0300

    Новый телефон

--- resources/views/components/header/controll.blade.php
+++ resources/views/components/header/controll.blade.php
@@ -1,10 +1,10 @@
 <section class="header_info">
     <div class="container">
-        <a href="{{route('home')}}" class="logo"></a>
-              <form role="search" method="get" class="searchform" id="searchform" action="{{ route('show_search_page') }}" >
-                  <input type="text" value="{{old("search_str")}}" name="search_str" id="search_str" />
-                  <button type="submit"></button>
-              </form>
+        <a href="{{ route('home') }}" class="logo"></a>
+        <form role="search" method="get" class="searchform" id="searchform" action="{{ route('show_search_page') }}">
+            <input type="text" value="{{ old('search_str') }}" name="search_str" id="search_str" />
+            <button type="submit"></button>
+        </form>
 
         <a id="counter_app" href="{{ route('bascet') }}" class="header__bascket-icon d-flex">
             <div class="bascket-icon-1 d-flex"></div>
@@ -14,14 +14,16 @@
         </a>
 
         <div class="header-contacts">
-            <a href="tel:+74712730499">  <span class=red> +7 (4712) 730-449 </span> </a>	<div class="address">г. Курск, ул. Верхняя Луговая, 6 </div>
+            <a href="tel:+79513112124"> <span class=red> +7 (951) 311-21-24 </span> </a>
+            <div class="address">г. Курск, ул. Верхняя Луговая, 6 </div>
         </div>
 
         <div class="header-contacts">
-            <a href="tel:+79510849233">  <span class=red> +7 (951) 084-92-33 </span> </a>	<div class="address">Отдел продаж</div>
+            <a href="tel:+79510849233"> <span class=red> +7 (951) 084-92-33 </span> </a>
+            <div class="address">Отдел продаж</div>
         </div>
 
 
 
     </div>
-  </section>
+</section>
