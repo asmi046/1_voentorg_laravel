568729e8d85ee8b57e668533e9db6b13f594c699
asmi046 <asmi046@gmail.com>
2026-05-13 00:46:47 +0300

    Шаблон письма

--- resources/views/mail/bascetmail.blade.php
+++ resources/views/mail/bascetmail.blade.php
@@ -1,14 +1,14 @@
 <h1>Письмо с сайта</h1>
-<p><strong>Имя:</strong> {{$formData['fio']}}</p>
-<p><strong>Телефон:</strong> {{$formData['phone']}}</p>
-<p><strong>E-mail:</strong> {{$formData['email']}}</p>
+<p><strong>Имя:</strong> {{ $formData['fio'] }}</p>
+<p><strong>Телефон:</strong> {{ $formData['phone'] }}</p>
+<p><strong>E-mail:</strong> {{ $formData['email'] }}</p>
 
-@if ()
-    <p><strong>Промокод:</strong> {{$formData['promo_code']}}</p>
-    <p><strong>Скидка по промокоду:</strong> {{$formData['promo_code_discount']}}</p>
+@if ($formData['promo_code'])
+    <p><strong>Промокод:</strong> {{ $formData['promo_code'] }}</p>
+    <p><strong>Скидка по промокоду:</strong> {{ $formData['promo_code_discount'] }}</p>
 @endif
 
-<p><strong>Комментарий:</strong> {{$formData['comment']}}</p>
+<p><strong>Комментарий:</strong> {{ $formData['comment'] }}</p>
 <table style="width:100%; border-top:1px solid black; border-left:1px solid black; border-spacing: 0;">
     <thead style="text-align:left;">
         <tr>
@@ -20,19 +20,22 @@
         </tr>
     </thead>
     <tbody>
-           @foreach ($formData['tovars'] as $item)
-
-
-                <tr>
-                    <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;"><img width="70" height="70" src="{{asset($item["tovar_content"]["img"])}}" alt=""></td>
-                    <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">{{$item["tovar_content"]["title"]}}<br/>
-                        Артикул: {{$item["product_sku"]}} / {{$item["product_id"]}}
-                        </span></td>
-                    <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">{{$item["price"]}} ₽</td>
-                    <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">{{$item["quentity"]}}</td>
-                    <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">{{(float)$item["quentity"] * (float)$item["price"]}} ₽</td>
-                </tr>
-            @endforeach
+        @foreach ($formData['tovars'] as $item)
+            <tr>
+                <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;"><img width="70"
+                        height="70" src="{{ asset($item['tovar_content']['img']) }}" alt=""></td>
+                <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">
+                    {{ $item['tovar_content']['title'] }}<br />
+                    Артикул: {{ $item['product_sku'] }} / {{ $item['product_id'] }}
+                    </span></td>
+                <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">
+                    {{ $item['price'] }} ₽</td>
+                <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">
+                    {{ $item['quentity'] }}</td>
+                <td style="padding: 5px; border-bottom:1px solid black; border-right:1px solid black;">
+                    {{ (float) $item['quentity'] * (float) $item['price'] }} ₽</td>
+            </tr>
+        @endforeach
     </tbody>
 </table>
-<h2>ИТОГО: {{$formData['amount']}} ₽</h2>
+<h2>ИТОГО: {{ $formData['amount'] }} ₽</h2>
